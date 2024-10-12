6e433114f05b93c32be2061233101e63b8843b34
Анна Кочерыгина <annvikky@gmail.com>
2024-10-12 15:38:41 +0800

    add private attributes, getters, setter and tests

--- src/main.py
+++ src/main.py
@@ -3,15 +3,37 @@ class Product:
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         """Метод для инициализации класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price  # приватный атрибут цены
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_info):
+        """Класс-метод для получения экземпляра класса из словаря."""
+        name = product_info.get("name")
+        description = product_info.get("description")
+        price = product_info.get("price")
+        quantity = product_info.get("quantity")
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """Геттер для обращения к приватному атрибуту цены."""
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        """Сеттер для изменения атрибута цены с проверкой на положительное значение."""
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = value
 
 
 class Category:
@@ -19,19 +41,37 @@ class Category:
 
     name: str
     description: str
-    products: list
+    __products: list[Product] = []
 
-    category_count = 0
-    product_count = 0
+    category_count: int = 0
+    product_count: int = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products: list[Product]):
         """Метод для инициализации класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products  # приватный атрибут списка товаров
 
         Category.category_count += 1
-        Category.product_count += len(products) if products else 0
+        Category.product_count += len(products)
+
+    def add_product(self, product: Product) -> None:
+        """Метод для добавления продукта."""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер для вывода информации о продукте в формате строки."""
+        products_str = ""
+
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
+
+    @property
+    def products_in_list(self):
+        return self.__products
 
 
 # if __name__ == "__main__":
@@ -82,3 +122,37 @@ class Category:
 #
 #     print(Category.category_count)
 #     print(Category.product_count)
+
+
+# if __name__ == "__main__":
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#     category1 = Category(
+#         "Смартфоны",
+#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#         [product1, product2, product3]
+#     )
+#
+#     print(category1.products)
+#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+#     category1.add_product(product4)
+#     print(category1.products)
+#     print(category1.product_count)
+#
+#     new_product = Product.new_product(
+#         {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+#          "quantity": 5})
+#     print(new_product.name)
+#     print(new_product.description)
+#     print(new_product.price)
+#     print(new_product.quantity)
+#
+#     new_product.price = 800
+#     print(new_product.price)
+#
+#     new_product.price = -100
+#     print(new_product.price)
+#     new_product.price = 0
+#     print(new_product.price)
